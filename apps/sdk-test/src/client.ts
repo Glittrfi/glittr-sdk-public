d7af29110d18825aa65a6fa79b24e7370c31188c
Edd <wahdanaedo@gmail.com>
2024-11-08 11:12:36 +0700

    add test for transfer glittr awareg

--- apps/sdk-test/src/client.ts
+++ apps/sdk-test/src/client.ts
@@ -1,6 +1,6 @@
-import { Account, GlittrSDK, txBuilder } from "@glittr-sdk/sdk";
+import { Account, BlockTxTuple, GlittrSDK, txBuilder } from "@glittr-sdk/sdk";
 
-async function main() {
+async function createFreeMintContract() {
   const NETWORK = "regtest";
 
   const client = new GlittrSDK({
@@ -25,9 +25,46 @@ async function main() {
   const txid = await client.createAndBroadcastTx({
     account: account.p2pkh(),
     tx: c,
-    outputs: []
+    outputs: [],
   });
   console.log("TXID : ", txid);
 }
 
-main();
+async function transfer() {
+  const NETWORK = "regtest";
+
+  const client = new GlittrSDK({
+    network: NETWORK,
+    electrumApi: "https://devnet-electrum.glittr.fi",
+    glittrApi: "https://devnet-core-api.glittr.fi",
+  });
+  const creatorAccount = new Account({
+    // mroHGEtVBLxKoo34HSHbHdmKz1ooJdA3ew
+    wif: "cW84FgWG9U1MpKvdzZMv4JZKLSU7iFAzMmXjkGvGUvh5WvhrEASj",
+    network: NETWORK,
+  });
+  const minterAccount = new Account({
+    // n3jM14MNfn1EEe1P8azEsmcPSP2BvykGLM
+    wif: "cMqUkLHLtHJ4gSBdxAVtgFjMnHkUi5ZXkrsoBcpECGrE2tcJiNDh",
+    network: NETWORK,
+  });
+
+  const contract: BlockTxTuple = [101869, 1];
+  const tx = txBuilder.transfer({
+    transfers: [
+      {
+        amount: "100",
+        asset: contract,
+        output: 1,
+      },
+    ],
+  });
+  const txid = await client.createAndBroadcastTx({
+    account: minterAccount.p2pkh(),
+    tx: tx,
+    outputs: [{ address: creatorAccount.p2pkh().address, value: 1000 }],
+  });
+  console.log("TXID : ", txid);
+}
+
+transfer();
