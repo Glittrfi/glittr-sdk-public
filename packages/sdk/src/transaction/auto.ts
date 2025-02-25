0fec3fc34ab607bb6f1b7bd152d4344dfadbf4fe
Vib-UX <vibhav@instaraise.io>
2025-02-24 20:22:51 -0700

    updates client interaction with amm custom_sum

--- packages/sdk/src/transaction/auto.ts
+++ packages/sdk/src/transaction/auto.ts
@@ -1,4 +1,11 @@
-import { BitcoinUTXO, BlockTxTuple, OpReturnMessage, Output, txBuilder, Varuint } from "..";
+import {
+  BitcoinUTXO,
+  BlockTxTuple,
+  OpReturnMessage,
+  Output,
+  txBuilder,
+  Varuint,
+} from "..";
 import { Account } from "../account";
 import { GlittrSDK } from "../client";
 import { getAssetTickers, getAssetUtxos } from "../helper/asset";
@@ -11,7 +18,7 @@ type TransferParams = {
   contractId: string;
   amount: string;
   receiver: string;
-}
+};
 
 class ContractDeployment {
   private client: GlittrSDK;
@@ -22,11 +29,32 @@ class ContractDeployment {
     this.account = account;
   }
 
-  async liquidityPoolInitiate(inputAsset: [string, string], inputAmount: [string, string]) {
+  /**
+   * Initiate a liquidity pool by deploying an MBA contract.
+   * @param inputAsset Array of two asset identifiers in the format "block:txIndex".
+   * @param inputAmount Array of two amounts required for each asset.
+   * @param ammModel (Optional) AMM model to use: "constant_product" or "constant_sum". Defaults to "constant_product".
+   */
+  async liquidityPoolInitiate(
+    inputAsset: [string, string],
+    inputAmount: [string, string],
+    ammModel: "constant_product" | "constant_sum" = "constant_product"
+  ) {
     const assetTickers = await getAssetTickers(this.client, inputAsset);
-    const asset1Utxos = await getAssetUtxos(this.client, this.account.p2wpkh().address, inputAsset[0]);
-    const asset2Utxos = await getAssetUtxos(this.client, this.account.p2wpkh().address, inputAsset[1]);
-    const utxos = await electrumFetchNonGlittrUtxos(this.client, this.account.p2wpkh().address)
+    const asset1Utxos = await getAssetUtxos(
+      this.client,
+      this.account.p2wpkh().address,
+      inputAsset[0]
+    );
+    const asset2Utxos = await getAssetUtxos(
+      this.client,
+      this.account.p2wpkh().address,
+      inputAsset[1]
+    );
+    const utxos = await electrumFetchNonGlittrUtxos(
+      this.client,
+      this.account.p2wpkh().address
+    );
 
     // Accumulate UTXOs and track excess
     let asset1Total = BigInt(0);
@@ -44,7 +72,7 @@ class ContractDeployment {
         txid: utxo.txid,
         vout: utxo.vout,
         value: utxo.value,
-        status: utxo.status
+        status: utxo.status,
       });
     }
 
@@ -55,7 +83,7 @@ class ContractDeployment {
         txid: utxo.txid,
         vout: utxo.vout,
         value: utxo.value,
-        status: utxo.status
+        status: utxo.status,
       });
     }
 
@@ -63,16 +91,16 @@ class ContractDeployment {
       throw new Error("Insufficient balance for liquidity pool");
     }
 
-    const [block1, txIndex1] = inputAsset[0].split(':').map(Number);
-    const [block2, txIndex2] = inputAsset[1].split(':').map(Number);
+    const [block1, txIndex1] = inputAsset[0].split(":").map(Number);
+    const [block2, txIndex2] = inputAsset[1].split(":").map(Number);
 
     const tx: OpReturnMessage = {
       contract_call: {
         call_type: {
           mint: {
-            pointer: 3
-          }
-        }
+            pointer: 3,
+          },
+        },
       },
       contract_creation: {
         contract_type: {
@@ -84,35 +112,36 @@ class ContractDeployment {
                 _mutable_assets: false,
                 input_assets: [
                   { glittr_asset: [block1!, txIndex1!] },
-                  { glittr_asset: [block2!, txIndex2!] }
+                  { glittr_asset: [block2!, txIndex2!] },
                 ],
                 mint_structure: {
                   proportional: {
-                    ratio_model: "constant_product"
-                  }
-                }
-              }
+                    // Use the provided ammModel ("constant_product" or "constant_sum")
+                    ratio_model: ammModel,
+                  },
+                },
+              },
             },
             burn_mechanism: {},
             swap_mechanism: {},
-            ticker: assetTickers.join('-')
-          }
-        }
+            ticker: assetTickers.join("-"),
+          },
+        },
       },
       transfer: {
         transfers: [
           {
             amount: (asset1Total - asset1Required).toString(),
             asset: [block1!, txIndex1!],
-            output: 1
+            output: 1,
           },
           {
             amount: (asset2Total - asset2Required).toString(),
             asset: [block2!, txIndex2!],
-            output: 2
-          }
-        ]
-      }
+            output: 2,
+          },
+        ],
+      },
     };
 
     const nonFeeInputs = [...usedUtxos1, ...usedUtxos2];
@@ -121,7 +150,7 @@ class ContractDeployment {
       { address: this.account.p2wpkh().address, value: 600 },
       { address: this.account.p2wpkh().address, value: 600 },
       { address: this.account.p2wpkh().address, value: 600 },
-    ]
+    ];
 
     const { inputs, outputs } = await addFeeToTx(
       this.client.network,
@@ -129,7 +158,7 @@ class ContractDeployment {
       utxos,
       nonFeeInputs,
       nonFeeOutputs
-    )
+    );
 
     return await this.client.createAndBroadcastRawTx({
       account: this.account.p2wpkh(),
@@ -138,7 +167,12 @@ class ContractDeployment {
     });
   }
 
-  async freeMint(ticker: string, divisibility: number, amountPerMint: string, supplyCap?: string) {
+  async freeMint(
+    ticker: string,
+    divisibility: number,
+    amountPerMint: string,
+    supplyCap?: string
+  ) {
     const _supplyCap = supplyCap;
     const tx: OpReturnMessage = {
       contract_creation: {
@@ -148,32 +182,45 @@ class ContractDeployment {
             live_time: 0,
             supply_cap: _supplyCap,
             ticker: ticker,
-            mint_mechanism: { free_mint: { amount_per_mint: amountPerMint, supply_cap: _supplyCap } }
-          }
+            mint_mechanism: {
+              free_mint: {
+                amount_per_mint: amountPerMint,
+                supply_cap: _supplyCap,
+              },
+            },
+          },
         },
       },
     };
 
-    const utxos = await electrumFetchNonGlittrUtxos(this.client, this.account.p2wpkh().address)
-    const nonFeeInputs: BitcoinUTXO[] = []
+    const utxos = await electrumFetchNonGlittrUtxos(
+      this.client,
+      this.account.p2wpkh().address
+    );
+    const nonFeeInputs: BitcoinUTXO[] = [];
     const nonFeeOutputs: Output[] = [
       { script: txBuilder.compile(tx), value: 0 },
-    ]
+    ];
     const { inputs, outputs } = await addFeeToTx(
       this.client.network,
       this.account.p2wpkh().address,
       utxos,
       nonFeeInputs,
       nonFeeOutputs
-    )
+    );
     return await this.client.createAndBroadcastRawTx({
       account: this.account.p2wpkh(),
       inputs,
-      outputs
-    })
+      outputs,
+    });
   }
 
-  async paidMint(ticker: string, divisibility: number, mechanism: PurchaseBurnSwap, supplyCap?: string) {
+  async paidMint(
+    ticker: string,
+    divisibility: number,
+    mechanism: PurchaseBurnSwap,
+    supplyCap?: string
+  ) {
     const _supplyCap = supplyCap ? supplyCap : undefined;
     const tx: OpReturnMessage = {
       contract_creation: {
@@ -188,31 +235,34 @@ class ContractDeployment {
                 input_asset: mechanism.input_asset,
                 pay_to_key: mechanism.pay_to_key,
                 ratio: mechanism.ratio,
-              }
-            }
-          }
+              },
+            },
+          },
         },
       },
     };
 
-    const utxos = await electrumFetchNonGlittrUtxos(this.client, this.account.p2wpkh().address)
-    const nonFeeInputs: BitcoinUTXO[] = []
+    const utxos = await electrumFetchNonGlittrUtxos(
+      this.client,
+      this.account.p2wpkh().address
+    );
+    const nonFeeInputs: BitcoinUTXO[] = [];
     const nonFeeOutputs: Output[] = [
       { script: txBuilder.compile(tx), value: 0 },
-    ]
+    ];
     const { inputs, outputs } = await addFeeToTx(
       this.client.network,
       this.account.p2wpkh().address,
       utxos,
       nonFeeInputs,
       nonFeeOutputs
-    )
+    );
 
     return await this.client.createAndBroadcastRawTx({
       account: this.account.p2wpkh(),
       inputs,
-      outputs
-    })
+      outputs,
+    });
   }
 }
 
@@ -225,11 +275,18 @@ class ContractCall {
     this.account = account;
   }
 
-  async mint(contractId: string, receiver: string, oracleMessage?: OracleMessageSigned): Promise<string> {
+  async mint(
+    contractId: string,
+    receiver: string,
+    oracleMessage?: OracleMessageSigned
+  ): Promise<string> {
     // TODO detect if the contract is paid mint or free mint
     const tx: OpReturnMessage = {
       contract_call: {
-        contract: [parseInt(contractId.split(":")[0]!), parseInt(contractId.split(":")[1]!)],
+        contract: [
+          parseInt(contractId.split(":")[0]!),
+          parseInt(contractId.split(":")[1]!),
+        ],
         call_type: {
           mint: {
             pointer: 1, // 0 is OpReturn
@@ -239,25 +296,28 @@ class ContractCall {
       },
     };
 
-    const utxos = await electrumFetchNonGlittrUtxos(this.client, this.account.p2wpkh().address)
-    const nonFeeInputs: BitcoinUTXO[] = []
+    const utxos = await electrumFetchNonGlittrUtxos(
+      this.client,
+      this.account.p2wpkh().address
+    );
+    const nonFeeInputs: BitcoinUTXO[] = [];
     const nonFeeOutputs: Output[] = [
       { script: txBuilder.compile(tx), value: 0 },
-      { address: receiver, value: 1000 }
-    ]
+      { address: receiver, value: 1000 },
+    ];
     const { inputs, outputs } = await addFeeToTx(
       this.client.network,
       this.account.p2wpkh().address,
       utxos,
       nonFeeInputs,
       nonFeeOutputs
-    )
+    );
 
     return await this.client.createAndBroadcastRawTx({
       account: this.account.p2wpkh(),
       inputs,
-      outputs
-    })
+      outputs,
+    });
   }
 }
 
@@ -275,74 +335,93 @@ export class GlittrTransaction {
   }
 
   async transfer(transfers: TransferParams[]): Promise<string> {
-    const allTransfers: {amount: Varuint | string, asset: BlockTxTuple, output: Varuint | number}[] = [];
-    const excessOutputs: {address: string, value: number}[] = [];
+    const allTransfers: {
+      amount: Varuint | string;
+      asset: BlockTxTuple;
+      output: Varuint | number;
+    }[] = [];
+    const excessOutputs: { address: string; value: number }[] = [];
 
     transfers.forEach((t, i) => {
       allTransfers.push({
         amount: t.amount.toString(),
-        asset: [parseInt(t.contractId.split(":")[0]!), parseInt(t.contractId.split(":")[1]!)],
+        asset: [
+          parseInt(t.contractId.split(":")[0]!),
+          parseInt(t.contractId.split(":")[1]!),
+        ],
         output: i + 1, // 0 is OpReturn
       });
     });
 
-    const utxos = await electrumFetchNonGlittrUtxos(this.client, this.account.p2tr().address)
+    const utxos = await electrumFetchNonGlittrUtxos(
+      this.client,
+      this.account.p2tr().address
+    );
 
-    const nonFeeInputs: BitcoinUTXO[] = []
+    const nonFeeInputs: BitcoinUTXO[] = [];
     for (const transfer of transfers) {
-      const assetRequired = BigInt(transfer.amount)
-      const assetUtxos = await getAssetUtxos(this.client, this.account.p2tr().address, transfer.contractId)
-      let assetTotal = BigInt(0)
+      const assetRequired = BigInt(transfer.amount);
+      const assetUtxos = await getAssetUtxos(
+        this.client,
+        this.account.p2tr().address,
+        transfer.contractId
+      );
+      let assetTotal = BigInt(0);
 
       for (const utxo of assetUtxos) {
-        if (assetTotal >= assetRequired) break
-        assetTotal += BigInt(utxo.assetAmount)
+        if (assetTotal >= assetRequired) break;
+        assetTotal += BigInt(utxo.assetAmount);
         nonFeeInputs.push({
           txid: utxo.txid,
           vout: utxo.vout,
           value: utxo.value,
-          status: utxo.status
-        })
+          status: utxo.status,
+        });
       }
 
       if (assetTotal < assetRequired) {
-        throw new Error(`Insufficient balance for asset ${transfer.contractId}. Required: ${assetRequired}, balance: ${assetTotal}`)
+        throw new Error(
+          `Insufficient balance for asset ${transfer.contractId}. Required: ${assetRequired}, balance: ${assetTotal}`
+        );
       }
 
-      const excessAssetValue = assetTotal - assetRequired
+      const excessAssetValue = assetTotal - assetRequired;
       if (excessAssetValue > 0) {
         // Add excess transfer to allTransfers array
         allTransfers.push({
-          asset: [parseInt(transfer.contractId.split(":")[0]!), parseInt(transfer.contractId.split(":")[1]!)],
+          asset: [
+            parseInt(transfer.contractId.split(":")[0]!),
+            parseInt(transfer.contractId.split(":")[1]!),
+          ],
           amount: excessAssetValue.toString(),
-          output: transfers.length + excessOutputs.length + 1
+          output: transfers.length + excessOutputs.length + 1,
         });
         // Add excess asset output to sender
         excessOutputs.push({
           address: this.account.p2tr().address,
-          value: 600
+          value: 600,
         });
       }
     }
 
     const tx: OpReturnMessage = {
       transfer: {
-        transfers: allTransfers
-      }
+        transfers: allTransfers,
+      },
     };
 
     const embed =
-    this.client.forceCompression || this.client.network != "regtest"
-      ? await txBuilder.compress(tx)
-      : txBuilder.compile(tx);
+      this.client.forceCompression || this.client.network != "regtest"
+        ? await txBuilder.compress(tx)
+        : txBuilder.compile(tx);
 
     const nonFeeOutputs: Output[] = [
       { script: embed, value: 0 },
-      ...transfers.map(t => ({
+      ...transfers.map((t) => ({
         address: t.receiver,
-        value: 600
+        value: 600,
       })),
-      ...excessOutputs
+      ...excessOutputs,
     ];
 
     const { inputs, outputs } = await addFeeToTx(
@@ -351,12 +430,12 @@ export class GlittrTransaction {
       utxos,
       nonFeeInputs,
       nonFeeOutputs
-    )
+    );
 
     return await this.client.createAndBroadcastRawTx({
       account: this.account.p2tr(),
       inputs,
-      outputs
+      outputs,
     });
   }
 }
